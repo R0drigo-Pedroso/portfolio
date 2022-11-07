a0684827b202d119f4d0d44f01cd1e3dcc4e8e72
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-07 15:32:19 -0300

    refaturando home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+
 import Home from "./Components/Home/Home";
+
+import estilos from "./Components/Home/Home.module.css";
 
 const App = () => {
   return (
     <React.Fragment>
-      <Container maxWidth="xl">
-        <Home />
+      <Container maxWidth="xl" className={estilos.display}>
+        <Box
+          sx={{
+            bgcolor: "#cfe8fc",
+            height: "90vh",
+            borderRadius: 1,
+          }}
+        >
+          <Home />
+        </Box>
       </Container>
     </React.Fragment>
   );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-import estilos from "./Home.module.css";
-
-import Caixa from "../Caixa/Caixa";
-
 const Home = () => {
   return (
-    <Caixa>
-      <h2 className={estilos.teste}>Teste</h2>
-    </Caixa>
+    <section>
+      <h2>teste</h2>
+      <h1>Teste h1</h1>
+    </section>
   );
 };
 
