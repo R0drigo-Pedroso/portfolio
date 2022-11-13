2825710511dc14825a9db63639bff1c426df2da6
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-13 17:01:15 -0300

    Atualização

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,23 +10,25 @@ const Home = () => {
     <Grid container spacing={2}>
       <Grid className={estilos.textoNome} item xs={12} md={6} sm={12}>
         <article className={estilos.boxTexto}>
-          <aside>
-            <h2>Olá, Me Chamo </h2>
-          </aside>
-          <aside>
-            <h1>Rodrigo Pedroso</h1>
-            <h4>Desenvolvedor Front-End</h4>
-          </aside>
+          <section>
+            <aside>
+              <h2>Olá, Me Chamo </h2>
+            </aside>
+            <aside>
+              <h1>Rodrigo Pedroso</h1>
+              <h4>Desenvolvedor Front-End</h4>
+            </aside>
 
-          <aside className={estilos.textoSobre}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam,
-            enim autem. Excepturi, similique eius, ad asperiores, atque sed et
-            ullam quae cum harum doloremque perferendis.
-          </aside>
+            <aside className={estilos.textoSobre}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam,
+              enim autem. Excepturi, similique eius, ad asperiores, atque sed et
+              ullam quae cum harum doloremque perferendis.
+            </aside>
 
-          <Button className={estilos.CTA} variant="contained">
-            mais sobre mim
-          </Button>
+            <Button className={estilos.CTA} variant="contained">
+              mais sobre mim
+            </Button>
+          </section>
         </article>
       </Grid>
       <Grid className={estilos.textoImg} item xs={12} md={6} sm={12}>
